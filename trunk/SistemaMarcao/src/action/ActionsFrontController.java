856eeb8f78525f84bda37c0c7fbb4a5775de43b0
pedrociarlini <pedrociarlini@1cd41cce-991a-0410-9b01-334d63f77eb0>
2006-08-25 20:48:36 +0000

    Adicionando implementação do padrão Front Controller.

---
+++ trunk/SistemaMarcao/src/action/ActionsFrontController.java
@@ -0,0 +1,31 @@
+package action;
+
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+import pedrociarlini.marcao.caixa.view.actions.SalvarTipoDePagamentoAction;
+
+/**
+ * Implementa o design pattern Front controller.
+ * @author pedro
+ *
+ */
+public class ActionsFrontController extends AbstractAction {
+	private static Map<String, Action> actions = new HashMap<String, Action>(10);
+
+	public ActionsFrontController() {
+		actions.put("SalvarTipoPagamento", new SalvarTipoDePagamentoAction());
+	}
+
+	public void actionPerformed(ActionEvent ev) {
+		String actionCommand = ev.getActionCommand();
+		Action action = actions.get(actionCommand);
+		synchronized (action) {
+			action.actionPerformed(ev);
+		}
+	}
+}
