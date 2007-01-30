c19319f92ad65e8ab36804afabc6400f85713a7c
pedrociarlini <pedrociarlini@1cd41cce-991a-0410-9b01-334d63f77eb0>
2007-01-30 18:10:29 +0000

    Inserir documentação do TinaPos.

---
+++ trunk/SistemaMarcao/docs/Marcão das ostras.mm
@@ -0,0 +1,16 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1170180465080" ID="Freemind_Link_768267900" MODIFIED="1170180482674" TEXT="Marc&#xe3;o das ostras">
+<node CREATED="1170180483752" ID="_" MODIFIED="1170180487471" POSITION="right" TEXT="Configura&#xe7;&#xe3;o das mesas">
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="Andar 0 - T&#xe9;rreo">
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="25 a 40"/>
+</node>
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="Andar 1 - M&#xfa;sica">
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="15 a 24"/>
+</node>
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="Andar 2 - Cima">
+<node CREATED="1170180490690" MODIFIED="1170180490690" TEXT="1 a 14"/>
+</node>
+</node>
+</node>
+</map>
